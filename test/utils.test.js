7b143d9cee3261b93bfd25c02dfb0bebf9f68f32
David Luecke <daff@neyeon.com>
2016-05-23 20:13:25 -0700

    Rename to feathers-reactive (#13)

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,7 +1,7 @@
 import assert from 'assert';
 import * as utils from '../src/utils';
 
-describe('feathers-rx utils', () => {
+describe('feathers-reactive utils', () => {
   it('promisify', done => {
     let promise = Promise.resolve('Hi');
     let obj = { test: 'me' };
