5f2cf9e9549311bc77127d1ab74eb53e804e7b76
Jan Lohage <git.lohage.de@j2l4e.de>
2016-08-01 17:48:44 +0200

    make find() query on subscription

--- src/list.js
+++ src/list.js
@@ -3,24 +3,29 @@ import { promisify } from './utils';
 export default function(Rx, events, options) {
   return function (params = {}) {
     const query = Object.assign({}, params.query);
-    const result = this._super.apply(this, arguments);
     const args = arguments;
+
+    const __super = this._super;
+
+    let result;
+
+    const source = Rx.Observable.create(observer => {
+      const _observer = observer;
+
+      result = __super.apply(this, arguments)
+        .then(res => {
+          _observer.next(res);
+          _observer.complete();
+        })
+        .catch(e => _observer.error(e));
+    });
 
     // Hack because feathers-query-params deletes stuff from `params.query`
     // Will be fixed in the next version
     params.query = query;
 
-    if(this._rx === false || params.rx === false || typeof result.then !== 'function') {
-      return result;
-    }
-
     options = Object.assign(options, this._rx, params.rx);
 
-    const source = Rx.Observable.fromPromise(result);
-
-    return promisify(
-      options.listStrategy.call(this, source, events, options, args),
-      result
-    );
+    return promisify(options.listStrategy.call(this, source, events, options, args), source.toPromise());
   };
 }
