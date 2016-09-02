cf403d1313c09111d440a1805315e5634a226b3f
David Luecke <daff@neyeon.com>
2016-09-01 21:10:28 -0700

    Test and fix for find being called only once (#30)

--- src/list.js
+++ src/list.js
@@ -4,28 +4,39 @@ export default function(Rx, events, settings) {
   return function (params = {}) {
     const query = Object.assign({}, params.query);
     const args = arguments;
-
     const __super = this._super;
 
-    let result;
+    let result = null;
 
     const source = Rx.Observable.create(observer => {
       const _observer = observer;
 
-      result = __super.apply(this, arguments)
-        .then(res => {
-          _observer.next(res);
-          _observer.complete();
-        })
-        .catch(e => _observer.error(e));
+      if(!result) {
+        result = __super.apply(this, arguments);
+
+        // Hack because feathers-query-params deletes stuff from `params.query`
+        // Will be fixed in the next version
+        params.query = query;
+      }
+
+      result.then(res => {
+        _observer.next(res);
+        _observer.complete();
+      })
+      .catch(e => _observer.error(e));
     });
 
-    // Hack because feathers-query-params deletes stuff from `params.query`
-    // Will be fixed in the next version
-    params.query = query;
-
     const options = getOptions(settings, this._rx, params.rx);
+    const promiseWrapper = {
+      then(... args) {
+        return source.toPromise().then(... args);
+      },
 
-    return promisify(options.listStrategy.call(this, source, events, options, args), source.toPromise());
+      catch(... args) {
+        return source.toPromise().catch(... args);
+      }
+    };
+
+    return promisify(options.listStrategy.call(this, source, events, options, args), promiseWrapper);
   };
 }
