aada96af5184e4fd00b60ecec4bafa9bb42a9542
Jan Lohage <git@j2L4e.de>
2018-05-15 11:29:05 +0200

    fix imports for typings

--- index.d.ts
+++ index.d.ts
@@ -5,9 +5,9 @@
 
 // TypeScript Version: 2.1
 
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { NullableId, Paginated, Params } from '@feathersjs/feathers';
-import { OperatorFunction } from 'rxjs/interfaces';
+import { OperatorFunction } from 'rxjs/internal/types';
 
 declare function FeathersReactive(options: FeathersReactive.Options): () => void;
 export = FeathersReactive;
