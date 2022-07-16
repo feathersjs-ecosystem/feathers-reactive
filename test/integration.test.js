d392a78f97b282832c2d757e51a82cc71899f4fd
Luc Claustres <luc.claustres@orange.fr>
2022-07-17 01:17:24 +0200

    chore: improved integration tests (#192)

--- test/integration.test.js
+++ test/integration.test.js
@@ -8,7 +8,7 @@ import rx from '../src';
 
 const app = feathers()
   .configure(socketio())
-  .use('/todos', memory());
+  .use('/messages', memory());
 
 app.on('connection', connection => app.channel('everybody').join(connection));
 app.publish(() => app.channel('everybody'));
@@ -18,18 +18,105 @@ describe('feathers-reactive integration', () => {
     await app.listen(3030);
   });
 
-  it('works with a client Feathers app', (done) => {
+  it('works with a client Feathers app initiating service operations', async () => {
     const socket = io('http://localhost:3030');
     const client = feathers()
       .configure(socketioClient(socket))
       .configure(rx({ idField: 'id' }));
 
-    client.service('todos')
+    let callCount = 0
+    const listener = client.service('messages')
       .watch({ listStrategy: 'smart' })
       .find({ query: {} })
       .subscribe(messages => {
         assert.ok(messages);
-        done();
-      }, done);
-  });
+        callCount++
+      });
+    await client.service('messages').create({
+      text: 'A test message'
+    });
+    await client.service('messages').patch(0, {
+      text: 'A patched test message'
+    });
+    await client.service('messages').update(0, {
+      id: 0,
+      text: 'An updated test message'
+    });
+    await client.service('messages').remove(0);
+    listener.unsubscribe()
+    // We should have 5 calls: first find + 4 events
+    assert.equal(callCount, 5);
+  }).timeout(5000);
+
+  it('works with a client Feathers app listening for service operations', async () => {
+    const socket = io('http://localhost:3030');
+    const client = feathers()
+      .configure(socketioClient(socket))
+      .configure(rx({ idField: 'id' }));
+
+    let callCount = 0
+    const listener = client.service('messages')
+      .watch({ listStrategy: 'smart' })
+      .find({ query: {} })
+      .subscribe(messages => {
+        assert.ok(messages);
+        callCount++
+      });
+    await app.service('messages').create({
+      text: 'A test message'
+    });
+    await app.service('messages').patch(1, {
+      text: 'A patched test message'
+    });
+    await app.service('messages').update(1, {
+      id: 1,
+      text: 'An updated test message'
+    });
+    await app.service('messages').remove(1);
+    listener.unsubscribe()
+    // We should have 5 calls: first find + 4 events
+    assert.equal(callCount, 5);
+  }).timeout(5000);
+
+  it('works with multiple client Feathers apps', async () => {
+    const socket = io('http://localhost:3030');
+    const client1 = feathers()
+      .configure(socketioClient(socket))
+      .configure(rx({ idField: 'id' }));
+    const client2 = feathers()
+      .configure(socketioClient(socket))
+      .configure(rx({ idField: 'id' }));
+
+    let callCount1 = 0, callCount2 = 0
+    const listener1 = client1.service('messages')
+      .watch({ listStrategy: 'smart' })
+      .find({ query: {} })
+      .subscribe(messages => {
+        assert.ok(messages);
+        callCount1++
+      });
+    const listener2 = client2.service('messages')
+      .watch({ listStrategy: 'smart' })
+      .find({ query: {} })
+      .subscribe(messages => {
+        assert.ok(messages);
+        callCount2++
+      });
+    await client1.service('messages').create({
+      text: 'A test message'
+    });
+    await client1.service('messages').patch(2, {
+      text: 'A patched test message'
+    });
+    await client2.service('messages').update(2, {
+      id: 2,
+      text: 'An updated test message'
+    });
+    await client2.service('messages').remove(2);
+    listener1.unsubscribe()
+    listener2.unsubscribe()
+    // We should have 5 calls: first find + 4 events
+    assert.equal(callCount1, 5);
+    assert.equal(callCount2, 5);
+  }).timeout(5000);
 });
