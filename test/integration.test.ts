c6a697013c2ba3a589b05520e9053dd635b7ed4b
Matthias Max <matthias.max@bitflower.net>
2023-06-01 23:10:16 +0200

    first working test

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -1,21 +1,20 @@
-import assert from 'assert';
 import { feathers } from '@feathersjs/feathers';
 import socketio from '@feathersjs/socketio';
 import socketioClient from '@feathersjs/socketio-client';
-import io from 'socket.io-client';
 import memory from 'feathers-memory';
 import { firstValueFrom } from 'rxjs';
-import rx from '../src';
+import io from 'socket.io-client';
+import { beforeAll, describe, expect, it } from 'vitest';
 
-const app = feathers()
-  .configure(socketio())
-  .use('/messages', memory());
+import { FeathersReactive } from '../src';
 
-app.on('connection', connection => app.channel('everybody').join(connection));
+const app = feathers().configure(socketio()).use('/messages', memory());
+
+app.on('connection', (connection) => app.channel('everybody').join(connection));
 app.publish(() => app.channel('everybody'));
 
 describe('feathers-reactive integration', () => {
-  before(async () => {
+  beforeAll(async () => {
     await app.listen(3030);
   });
 
@@ -23,14 +22,15 @@ describe('feathers-reactive integration', () => {
     const socket = io('http://localhost:3030');
     const client = feathers()
       .configure(socketioClient(socket))
-      .configure(rx({ idField: 'id' }));
+      .configure(FeathersReactive({ idField: 'id' }));
 
     let callCount = 0;
-    const listener = client.service('messages')
+    const listener = client
+      .service('messages')
       .watch({ listStrategy: 'smart' })
       .find({ query: {} })
-      .subscribe(messages => {
-        assert.ok(messages);
+      .subscribe((messages) => {
+        expect(messages).toBeDefined();
         callCount++;
       });
 
@@ -47,21 +47,22 @@ describe('feathers-reactive integration', () => {
     await client.service('messages').remove(message.id);
     listener.unsubscribe();
     // We should have 5 calls: first find + 4 events
-    assert.equal(callCount, 5);
+    expect(callCount).toBe(5);
   });
 
   it('works with a client Feathers app listening for service operations', async () => {
     const socket = io('http://localhost:3030');
     const client = feathers()
       .configure(socketioClient(socket))
-      .configure(rx({ idField: 'id' }));
+      .configure(FeathersReactive({ idField: 'id' }));
 
     let callCount = 0;
-    const find = client.service('messages')
+    const find = client
+      .service('messages')
       .watch({ listStrategy: 'smart' })
       .find({ query: {} });
-    const listener = find.subscribe(messages => {
-      assert.ok(messages);
+    const listener = find.subscribe((messages) => {
+      expect(messages).toBeDefined();
       callCount++;
     });
 
@@ -81,37 +82,41 @@ describe('feathers-reactive integration', () => {
     });
 
     await app.service('messages').remove(message.id);
-    await new Promise(resolve => client.service('messages').once('removed', resolve));
+    await new Promise((resolve) =>
+      client.service('messages').once('removed', resolve)
+    );
 
     listener.unsubscribe();
     // We should have 5 calls: first find + 4 events
-    assert.equal(callCount, 5);
+    expect(callCount).toBe(5);
   });
 
   it('works with multiple client Feathers apps', async () => {
     const socket = io('http://localhost:3030');
     const client1 = feathers()
       .configure(socketioClient(socket))
-      .configure(rx({ idField: 'id' }));
+      .configure(FeathersReactive({ idField: 'id' }));
     const client2 = feathers()
       .configure(socketioClient(socket))
-      .configure(rx({ idField: 'id' }));
+      .configure(FeathersReactive({ idField: 'id' }));
 
     let callCount1 = 0;
     let callCount2 = 0;
 
-    const find1 = client1.service('messages')
+    const find1 = client1
+      .service('messages')
       .watch({ listStrategy: 'smart' })
       .find({ query: {} });
-    const listener1 = find1.subscribe(messages => {
-      assert.ok(messages);
+    const listener1 = find1.subscribe((messages) => {
+      expect(messages).toBeDefined();
       callCount1++;
     });
-    const find2 = client2.service('messages')
+    const find2 = client2
+      .service('messages')
       .watch({ listStrategy: 'smart' })
       .find({ query: {} });
-    const listener2 = find2.subscribe(messages => {
-      assert.ok(messages);
+    const listener2 = find2.subscribe((messages) => {
+      expect(messages).toBeDefined();
       callCount2++;
     });
 
@@ -137,7 +142,7 @@ describe('feathers-reactive integration', () => {
     listener1.unsubscribe();
     listener2.unsubscribe();
     // We should have 5 calls: first find + 4 events
-    assert.equal(callCount1, 5);
-    assert.equal(callCount2, 5);
+    expect(callCount1).toBe(5);
+    expect(callCount2).toBe(5);
   });
 });
